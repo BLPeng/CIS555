c119f180a965c063933819a428b85728f9495ebd
eureka <lakayiiii@gmail.com>
2015-02-05 18:04:08 -0500

    request handle arch implemented

--- HW1/src/edu/upenn/cis/cis455/webserver/HTTPRequestParser.java
+++ HW1/src/edu/upenn/cis/cis455/webserver/HTTPRequestParser.java
@@ -1,5 +1,106 @@
 package edu.upenn.cis.cis455.webserver;
 
-public class HTTPRequestParser {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+import org.apache.log4j.Logger;
+
+public class HTTPRequestParser {
+	
+	static final Logger logger = Logger.getLogger(HTTPRequestParser.class);	
+	private RequestLine initialLine;	//first line of req
+	private CODE code;			//parse code
+		
+	public RequestLine getInitialLine() {
+		return initialLine;
+	}
+
+	public CODE getCode() {
+		return code;
+	}
+
+	public enum CODE {
+		BADREQ, BADDIR, SHUTDOWN, CONTROL, PARSE, NONE, NOFOUND
+	}
+	
+	public CODE parseHttpRequest(Socket socket){
+		
+		BufferedReader in;
+
+		try {
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			//handle the initial line
+			String initLine = in.readLine();
+			initialLine = new RequestLine(initLine);
+			if (initialLine.valid == false){
+				code = CODE.BADREQ;
+				return CODE.BADREQ;
+			}	
+			
+			if (filterInvalidRequest(initialLine)){
+				code = CODE.BADREQ;
+				return CODE.BADREQ; 
+			}
+			
+			if ("/shutdown".equals(initialLine.url.toLowerCase())){
+				//shotdown server
+				code = CODE.SHUTDOWN;
+				return CODE.SHUTDOWN; 
+			}
+			
+			if ("/control".equals(initialLine.url.toLowerCase())){
+				code = CODE.CONTROL;
+				return CODE.CONTROL;
+			}
+			
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+
+		return CODE.NONE;
+		
+	}
+	
+
+	// filter out invalid request
+	private boolean filterInvalidRequest(RequestLine initialLine){
+		
+	    System.out.println(initialLine.oriStr);
+		if (!"GET".equals(initialLine.method) && !"HEAD".equals(initialLine.method) ){
+			return true;
+		}
+		//TODO
+		return false;
+	}
+	
+	class RequestLine{
+		
+		String method;
+		String url;
+		String protocol;
+		String oriStr;
+		boolean valid;
+		
+		public RequestLine(String line){
+			
+			oriStr = line;
+			valid = true;
+			String[] tmp = line.split(" ");
+			if (tmp.length != 3){
+				valid = false;
+			}else{
+				method = tmp[0];
+				url = tmp[1];
+				protocol = tmp[2];
+			}
+		}
+	
+	}
+	
 }
--- HW1/src/edu/upenn/cis/cis455/webserver/WorkerThread.java
+++ HW1/src/edu/upenn/cis/cis455/webserver/WorkerThread.java
@@ -1,12 +1,18 @@
 package edu.upenn.cis.cis455.webserver;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+
 import org.apache.log4j.Logger;
+
+import edu.upenn.cis.cis455.webserver.HTTPRequestParser.CODE;
 
 
 public class WorkerThread extends Thread{
 
 	private MyBlockingQueue<Socket> requestQueue;
+	private Socket task;
 	private Boolean run;
 	static final Logger logger = Logger.getLogger(WorkerThread.class);	
 	
@@ -19,8 +25,16 @@ public class WorkerThread extends Thread{
 		while (run){
 			
 			try {
-				Socket task = requestQueue.get();
+				task = requestQueue.get();
 				logger.info("Handle task");
+				HTTPRequestParser requestParser = new HTTPRequestParser();
+				CODE code = requestParser.parseHttpRequest(task);
+				reponseError(requestParser, code, task);
+				
+				
+				
+				closeSocket();
+				
 			} catch (InterruptedException e) {
 				logger.error("Failed to get task");
 				e.printStackTrace();
@@ -31,11 +45,71 @@ public class WorkerThread extends Thread{
 	}
 	
 	public void stopThread(){
-		
+		//TODO
 		synchronized (run){
 			this.run = false;
 		}
 
+	}
+	
+	
+	private void handleRequest(HTTPRequestParser requestParser, Socket socket){
+		
+		if (requestParser == null)	return;
+		CODE code = requestParser.getCode();
+		switch (code){
+		case BADDIR:
+		
+			
+		case BADREQ:
+			
+			
+		}
+		
+	}
+	
+	
+	private void reponseError(HTTPRequestParser requestParser, CODE code, Socket socket){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(requestParser.getInitialLine().method);
+		sb.append(" ");
+		switch (code){
+		case BADREQ:{
+			sb.append(" 400 "); sb.append(" Bad request method!");
+			try {
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				String res = sb.toString();
+				out.println(res);
+				out.close();
+			} catch (IOException e) {
+				logger.error("Error");
+				e.printStackTrace();
+			}
+			break;
+		}
+			
+		case BADDIR:
+		}
+		
+		
+	}
+	
+	private void shutdownServer(Socket socket){
+		
+	}
+	
+	private void closeSocket(){
+		
+		try {
+			if (task != null){
+				task.close();
+			}		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
