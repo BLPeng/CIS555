f9c494536e9ad1674f13a2c00d8e70b373a42ec7
eureka <lakayiiii@gmail.com>
2015-04-09 11:41:44 -0400

    sha-1

--- HW3/src/edu/upenn/cis455/mapreduce/worker/MapThread.java
+++ HW3/src/edu/upenn/cis455/mapreduce/worker/MapThread.java
@@ -1,5 +1,8 @@
 package edu.upenn.cis455.mapreduce.worker;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -24,11 +27,18 @@ public class MapThread extends Thread{
 					mapThreadPool.decreaseCnt();
 				} else {
 					if (line != null) {
-						
+						String hash = sha1(line);
+						int index = getWorkerIndex(hash);
 					}
 				}
 			} catch (InterruptedException e) {
 		//		e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
@@ -38,6 +48,23 @@ public class MapThread extends Thread{
 		this.interrupt();
 	}
 	
+	//assume num of workers less than 65536
+	private int getWorkerIndex(String hash) {
+		String head = hash.substring(0, 4);
+		int val = Integer.parseInt(head, 16); 
+		int range = 65536 / mapThreadPool.getThreadPoolSize();
+		int idx = val / range;
+		return idx;
+	}
 	
+	private String sha1(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA-1");
+        byte[] result = mDigest.digest(input.getBytes("UTF-8"));
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(String.format("%02X ", result[i]));
+        }         
+        return sb.toString();
+    }
 	
 }
